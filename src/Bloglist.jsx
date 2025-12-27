c8e89f9da785e9e71e166adff4276202795461c4
Ray Lopez <raylopez509@gmail.com>
2025-12-26 22:09:46 -0800

    added fetch database api, also added props to Blogpost from the database

--- src/Bloglist.jsx
+++ src/Bloglist.jsx
@@ -1,24 +1,39 @@
 import Blogpost from './Blogpost'
 import './Bloglist.css'
-// import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 
 function Bloglist() {
 
-  // const [postData, setPostData] = useState([]);
+  const [postData, setPostData] = useState([]);
 
-  // useEffect(() => {
-  //   fetch('http://localhost:3000/posts').then(
-  //     res => {
-  //       return res.json();
-  //     }
-  //   ).then(data => {
-  //     setPostData(data);
-  //   })
-  // }, []);
+  useEffect(() => {
+    fetch('http://localhost:3000/posts').then(
+      res => {
+        return res.json();
+      }
+    ).then(data => {
+      setPostData(data);
+    })
+  }, []);
+
+  console.log(postData);
+
+  if (postData.length === 0) {
+    return <></>
+  }
 
   return (
     <div className='bloglist'>
-      <Blogpost />
+      {
+        postData.map(post => (
+          <Blogpost 
+            title = {post.title}
+            content = {post.content}
+            tags = {post.tags}
+            date_created={post.date_created}
+          />
+        ))
+      }
     </div>
   )
 }
