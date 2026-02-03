d9e2f9bd642c736ae64da54286e3631a34d0f8fe
Ray Lopez <raylopez509@gmail.com>
2026-02-03 15:20:16 -0800

    added tiptap editor

---
+++ src/Editor.jsx
@@ -0,0 +1,26 @@
+import { useEditor, EditorContent, EditorContext } from '@tiptap/react'
+import { FloatingMenu, BubbleMenu } from '@tiptap/react/menus'
+import StarterKit from '@tiptap/starter-kit'
+import { useMemo } from 'react'
+import MenuBar from './MenuBar'
+import PostButton from './PostButton'
+
+function Editor() {
+  const editor = useEditor({
+    extensions: [StarterKit], // define your extension array
+    // content: '<p>Write about anything</p>', // initial content
+  })
+
+  // Memoize the provider value to avoid unnecessary re-renders
+  const providerValue = useMemo(() => ({ editor }), [editor])
+
+  return (
+    <EditorContext.Provider value={providerValue}>
+      <MenuBar editor={editor}/>
+      <EditorContent editor={editor} />
+      <PostButton editor={editor} />
+    </EditorContext.Provider>
+  )
+}
+
+export default Editor
