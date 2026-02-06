c69d857a36f42a429e47c2736300a7b3e6272905
Ray Lopez <raylopez509@gmail.com>
2026-02-06 15:28:55 -0800

    added fetch code to send data to database

--- src/AddPostModal.jsx
+++ src/AddPostModal.jsx
@@ -7,23 +7,66 @@ function AddPostModal({ onClose }) {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
 
+  const headers = new Headers();
+  headers.append("Content-Type", "application/json");
+
+  const body = JSON.stringify({
+    "title": title,
+    "content": content,
+    "tags": [
+      "blog",
+      "test"
+    ],
+    "date_created": "CURRENT_TIMESTAMP",
+    "owner": "ray"
+  })
+
+  const requestOptions = {
+    method: "POST",
+    headers: headers,
+    body: body,
+  };
+
+  const submitPost = (e) => {
+    // e.preventDefault();
+
+  fetch("http://localhost:3000/create", requestOptions)
+    .then((response) => response.text())
+    .then((result) => console.log(result))
+    .catch((error) => console.error(error));
+
+    onClose();
+  }
+
   return (
     <section className='modal'>
       Add Post
-      <input placeholder='Title'
+      {/* <input placeholder='Title'
         value={title}
         onChange={(e) => setTitle(e.target.value)}
-      ></input>
+      ></input> */}
       {/* <Editor /> */}
-      <textarea placeholder='Make a Post!'
+      {/* <textarea placeholder='Make a Post!'
         value={content}
         onChange={(e) => setContent(e.target.value)}
-      ></textarea>
-      <PostButton
+      ></textarea> */}
+      {/* <PostButton
         title={title}
         content={content}
-      />
-      <button onClick={onClose}>Cancel</button>
+      /> */}
+      {/* <button onClick={onClose}>Cancel</button> */}
+      <form onSubmit={submitPost}>
+        <input placeholder='Title'
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        ></input>
+        <textarea placeholder='Make a Post!'
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+        ></textarea>
+        <button type='submit'>Post</button>
+        <button onClick={onClose}>Cancel</button>         
+      </form>
     </section>
   );
 }
