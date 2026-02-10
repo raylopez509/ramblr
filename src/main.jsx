2d87d7d7a92a395b85849c2ea7e4030e33d1bff0
Ray Lopez <raylopez509@gmail.com>
2026-02-10 11:06:45 -0800

    changed to one App component which will hold the button and bloglist

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Bloglist from './Bloglist.jsx'
-import Button from './Button.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Button />
-    <Bloglist />
+    <App />
   </StrictMode>,
 )
 
