e24d21d5da14a4272c8741e425ca8bd6f65c9b31
Ray Lopez <raylopez509@gmail.com>
2026-02-11 10:15:26 -0800

    removed commented out code

--- src/App.jsx
+++ src/App.jsx
@@ -8,17 +8,9 @@ function App() {
   const [postData, setPostData] = useState([]);
 
   const fetchPosts = async () => {
-    // fetch('http://localhost:3000/posts').then(
-    //   res => {
-    //     return res.json();
-    //   }
-    // ).then(data => {
-    //   setPostData(data);
-    // })
     const res = await fetch('http://localhost:3000/posts');
     const data = await res.json();
     setPostData(data);
-
   }
 
   useEffect(() => {
