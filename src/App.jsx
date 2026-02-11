ca222532815dded9e375ded78fb42d65333727aa
Ray Lopez <raylopez509@gmail.com>
2026-02-10 16:51:39 -0800

    removed boilerplate code and moved the fetchposts function up here

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useState, useEffect } from 'react';
+import Bloglist from './Bloglist.jsx'
+import Button from './Button.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [postData, setPostData] = useState([]);
+
+  const fetchPosts = async () => {
+    // fetch('http://localhost:3000/posts').then(
+    //   res => {
+    //     return res.json();
+    //   }
+    // ).then(data => {
+    //   setPostData(data);
+    // })
+    const res = await fetch('http://localhost:3000/posts');
+    const data = await res.json();
+    setPostData(data);
+
+  }
+
+  useEffect(() => {
+    fetchPosts()
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Button onPostCreated={fetchPosts}/>
+      <Bloglist postData={postData}/>
     </>
   )
 }
