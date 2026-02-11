39072687cd3ca7c63d5be296e383210c786300b9
Ray Lopez <raylopez509@gmail.com>
2026-02-10 16:53:37 -0800

    Added the onPostCreated function to the AddPostModal component

--- src/Button.jsx
+++ src/Button.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import { createPortal } from 'react-dom'
 import AddPostModal from './AddPostModal'
 
-function Button() {
+function Button({ onPostCreated }) {
 
   const [showModal, setShowModal] = useState(false); 
 
@@ -10,7 +10,9 @@ function Button() {
     <>
       <button onClick={() => setShowModal(true)}>Add</button>
       {showModal && createPortal(
-        <AddPostModal onClose={() => setShowModal(false)} />,
+        <AddPostModal onClose={() => setShowModal(false)}
+          onPostCreated={onPostCreated}
+        />,
         document.body
       )}
     </>
