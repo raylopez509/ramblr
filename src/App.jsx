8fdca20a3a05ab9c01c9610c41af3a742706afbb
Ray Lopez <raylopez509@gmail.com>
2026-02-26 16:57:54 -0800

    added states for pagination

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,49 @@ import { useState, useEffect } from 'react';
 import Bloglist from './Bloglist.jsx'
 import AddPostButton from './AddPostButton.jsx'
 import ListPostsButton from './ListPostsButton.jsx';
+import Pagination from './Pagination.jsx';
 
 function App() {
 
   const [postData, setPostData] = useState([]);
 
+  const [pageNum, setPageNum] = useState(1);
+  const [showPrevButton, setShowPrevButton] = useState(false);
+  const [showNextButton, setShowNextButton] = useState(false);
+
+  const postLimit = 5;
+  
+  function checkButtonStatus(data, num){
+    if(num == 1) {
+      setShowPrevButton(false);
+    }
+    else {
+      setShowPrevButton(true);
+    }
+    if(data.length < postLimit) {
+      setShowNextButton(false);
+    }
+    else {
+      setShowNextButton(true);
+    }
+  }
+  
+  function changePageNum(num) {
+    setPageNum(num);
+  }
+
   const fetchPosts = async () => {
-    const res = await fetch('http://localhost:3000/posts');
+    const query = 'http://localhost:3000/posts' + '?limit=' + postLimit + '&page=' + pageNum;
+    console.log(query);
+    const res = await fetch(query);
     const data = await res.json();
     setPostData(data);
+    checkButtonStatus(data, pageNum);
   }
 
   useEffect(() => {
-    fetchPosts()
-  }, []);
+    fetchPosts();   
+  }, [pageNum]);
 
   return (
     <>
@@ -25,6 +54,7 @@ function App() {
       <ListPostsButton postData={postData}
       refreshPosts={fetchPosts}/>
       <Bloglist postData={postData}/>
+      <Pagination pageNum={pageNum} showPrevButton={showPrevButton} showNextButton={showNextButton} changePageNum={changePageNum}/>
     </>
   )
 }
